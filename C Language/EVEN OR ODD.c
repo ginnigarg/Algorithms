8399dbe0f755bad3ec62e90a4a05c59efec815a2
Ayush Singla <32064132+ayusin51@users.noreply.github.com>
2019-10-11 00:24:16 +0530

    Update EVEN OR ODD.c

--- C Language/EVEN OR ODD.c
+++ C Language/EVEN OR ODD.c
@@ -3,13 +3,6 @@ int main()
 {
  int num;
  printf ("Enter the number : ");
- scanf ("%d",&num);
- if (num%2==0)
-   {
-     printf ("%d is even \n");
-   }
- else
-   {
-     printf ("%d is odd \n");
-   }
+ scanf("%d", &num);
+ num % 2 == 0 ? printf ("%d is even \n") : printf ("%d is odd \n");
 }
