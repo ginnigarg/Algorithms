8f39b7c2b0ad42aea7293f59791b7d880d26c0e2
Ayush Singla <32064132+ayusin51@users.noreply.github.com>
2020-10-02 11:45:46 +0530

    Update MAGIC NUMBERS.c

--- C Language/MAGIC NUMBERS.c
+++ C Language/MAGIC NUMBERS.c
@@ -2,13 +2,13 @@
 #include<math.h>
 int main()
 {
-	int i=1,j=0,arr[100];
-	while (j<100)
+	int i = 1,j = 0,arr[100];
+	while (j < 100)
 	{
-		arr[j]=pow(5,i++);
-		int a=j,k;
+		arr[j] = pow(5,i++);
+		int a = j, k;
 		j++;
-		for (k=0;k<a;k++)
+		for (k = 0;k < a;k++)
 		{
 			arr[j++]=arr[a]+arr[k];
 		}
